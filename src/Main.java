e0f701aeb3c0548eb580168409ed8fdcd6072ed8
Milyukov Sergey <serrguk@gmail.om>
2023-02-27 22:00:48 +0700

    line "*ССЛОЖЕНИЕ СКОРОСТЕЙ С УЧЁТОМ РЕЛЯТИВИСТСКОГО ЭФФЕКТА*" added

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,8 @@ public class Main {
     public static void main(String[] args) {
 
         //SPEED_OF_LIGHT = 1_079_252_848.8 kph
-        System.out.println("Строка для практики по слиянию веток");
+
+        System.out.println("\n*СЛОЖЕНИЕ СКОРОСТЕЙ С УЧЁТОМ РЕЛЯТИВИСТСКОГО ЭФФЕКТА*\n");
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите скорость первого объекта в км/ч: ");
